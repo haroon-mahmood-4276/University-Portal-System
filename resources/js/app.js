d0651bdb386740e3cbf903015a8558608e678ab8
haroon-mahmood-4276 <haroon.mahmood.4276@gmail.com>
2020-12-17 18:06:58 +0500

    Vue-Front

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
+import Vue from 'vue';
+import {
+    BootstrapVue,
+    IconsPlugin
+} from 'bootstrap-vue';
+import VueRoutes from './router';
+import vueapp from "./components/App.vue";
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        vueapp
+    },
+    VueRoutes
+});
--- resources/views/Others/Master.blade.php
+++ resources/views/Others/Master.blade.php
@@ -7,45 +7,34 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('PageTitle') - UNi Portal Management System</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 </head>
 
 <body>
+    <div id="app">
+        <div>
+            {{ View::make('Others.MainHeader') }}
+        </div>
 
-    {{-- Header --}}
-    <div>
-        {{ View::make('Others.MainHeader') }}
+
+        <div>
+            @yield('content')
+        </div>
+
+        {{ View::make('Others.Footer') }}
+        <vueapp></vueapp>
     </div>
 
-    {{-- Middle Container --}}
-    <div>
-        @yield('content')
-    </div>
-
-    {{-- Footer --}}
-    {{ View::make('Others.Footer') }}
-
-
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-    {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script> --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
 
     <script src="https://kit.fontawesome.com/dfc786dc18.js" crossorigin="anonymous"></script>
 
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/unijs.js') }}"></script>
 </body>
 
 </html>
